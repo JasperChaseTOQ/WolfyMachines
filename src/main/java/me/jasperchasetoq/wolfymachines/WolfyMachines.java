72fb5e2107af6fb89fcc2b732962119874c6a139
JasperChaseTOQ <103849596+JasperChaseTOQ@users.noreply.github.com>
2022-09-10 15:27:57 -0400

    Update WolfyMachines.java

--- src/main/java/me/jasperchasetoq/wolfymachines/WolfyMachines.java
+++ src/main/java/me/jasperchasetoq/wolfymachines/WolfyMachines.java
@@ -1,5 +1,7 @@
 package me.jasperchasetoq.wolfymachines;
 
+import io.github.thebusybiscuit.slimefun4.libraries.dough.updater.GitHubBuildsUpdater;
+import jdk.internal.platform.Metrics;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
 import org.bukkit.inventory.ItemStack;
@@ -13,72 +15,53 @@ import io.github.thebusybiscuit.slimefun4.api.SlimefunAddon;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.config.Config;
 import io.github.thebusybiscuit.slimefun4.libraries.dough.items.CustomItemStack;
 
+import java.io.File;
+
 public class WolfyMachines extends JavaPlugin implements SlimefunAddon {
+
 
     @Override
     public void onEnable() {
-        // Read something from your config.yml
+
         Config cfg = new Config(this);
 
-        if (cfg.getBoolean("options.auto-update")) {
-            // You could start an Auto-Updater for example
+        WolfyMachinesItemsSetup.setup(this);
+        if (!new File(getDataFolder(), "config.yml").exists()) {
+            saveDefaultConfig();
         }
 
-        /*
-         * 1. Creating a new Category
-         * This Category will use the following ItemStack
-         */
-        ItemStack itemGroupItem = new CustomItemStack(Material.DIAMOND, "&4Addon Category", "", "&a> Click to open");
+        if (getConfig().getBoolean("options.auto-update") && getDescription().getVersion().startsWith("DEV - ")) {
+            new GitHubBuildsUpdater(this, getFile(), "JasperChaseTOQ/WolfyMachines/master").start();
 
-        // Give your Category a unique id.
-        NamespacedKey itemGroupId = new NamespacedKey(this, "addon_category");
-        ItemGroup itemGroup = new ItemGroup(itemGroupId, itemGroupItem);
-
-        /*
-         * 2. Create a new SlimefunItemStack
-         * This class has many constructors, it is very important
-         * that you give each item a unique id.
-         */
-        SlimefunItemStack slimefunItem = new SlimefunItemStack("COOL_DIAMOND", Material.DIAMOND, "&4Cool Diamond", "&c+20% Coolness");
-
-        /*
-         * 3. Creating a Recipe
-         * The Recipe is an ItemStack Array with a length of 9.
-         * It represents a Shaped Recipe in a 3x3 crafting grid.
-         * The machine in which this recipe is crafted in is specified
-         * further down as the RecipeType.
-         */
-        ItemStack[] recipe = { new ItemStack(Material.EMERALD), null, new ItemStack(Material.EMERALD), null, new ItemStack(Material.DIAMOND), null, new ItemStack(Material.EMERALD), null, new ItemStack(Material.EMERALD) };
-
-        /*
-         * 4. Registering the Item
-         * Now you just have to register the item.
-         * RecipeType.ENHANCED_CRAFTING_TABLE refers to the machine in
-         * which this item is crafted in.
-         * Recipe Types from Slimefun itself will automatically add the recipe to that machine.
-         */
-        SlimefunItem item = new SlimefunItem(itemGroup, slimefunItem, RecipeType.ENHANCED_CRAFTING_TABLE, recipe);
-        item.register(this);
+            int pluginId = 16400; // <-- Replace with the id of your plugin!
+            Metrics metrics = new Metrics(this, pluginId);
+        }
     }
-
     @Override
     public void onDisable() {
         // Logic for disabling the plugin...
     }
-
     @Override
     public String getBugTrackerURL() {
-        // You can return a link to your Bug Tracker instead of null here
-        return null;
+        return "https://github.com/JasperChaseTOQ/WolfyMachines/issues";
     }
 
+    @Nonnull
     @Override
     public JavaPlugin getJavaPlugin() {
-        /*
-         * You will need to return a reference to your Plugin here.
-         * If you are using your main class for this, simply return "this".
-         */
         return this;
     }
 
+    private static WolfyMachines instance;
+
+    public WolfyMachines() {
+        instance = this;
+    }
+
+    public static WolfyMachines getInstance() {
+        return instance;
+    }
+    public static String getVersion() {
+        return instance.getDescription().getVersion();
+    }
 }
